a41908464caff4256d1d56ad98e38395eb3b2e19
JuanJavierRuiz <123001800+JuanJavierRuiz@users.noreply.github.com>
2023-01-23 12:05:36 -0500

    Added Another Sun

--- Picture.java
+++ Picture.java
@@ -15,6 +15,7 @@ public class Picture
     private Square window;
     private Triangle roof;
     private Circle sun;
+    private Circle sun2;
     private boolean drawn;
 
     /**
@@ -26,6 +27,7 @@ public class Picture
         window = new Square();
         roof = new Triangle();  
         sun = new Circle();
+        sun2 = new Circle();
         drawn = false;
     }
 
@@ -57,6 +59,14 @@ public class Picture
             sun.changeSize(80);
             sun.makeVisible();
             drawn = true;
+            
+            sun2.changeColor("yellow");
+            sun2.moveHorizontal(50);
+            sun2.moveVertical(-40);
+            sun2.changeSize(80);
+            sun2.makeVisible();
+            drawn = true;
+            
         }
     }
 
